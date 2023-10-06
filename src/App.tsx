dda2fb7c93feb9a39d86b4a36498b0e157637479
muzi55 <muzi5@kakao.com>
2023-10-06 23:57:01 +0900

    setTime 예시 추가

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,18 @@ function App() {
     setClientXY([e.clientX, e.clientY]);
     console.log([e.clientX, e.clientY]);
   };
+
   useEffect(() => {
     window.addEventListener("mousemove", handleMouseMove);
 
-    return () => window.removeEventListener("mousemove", handleMouseMove);
+    const timer = setTimeout(() => {
+      alert("타이머");
+    }, 2000);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      clearTimeout(timer);
+    };
   }, []);
 
   return (
@@ -18,6 +26,8 @@ function App() {
       <h2>홈</h2>
       <p>홈 페이지입니다.</p>
       <p>마우스 X, Y 좌표 {`${clientXY[0]}, ${clientXY[1]}`}</p>
+
+      <button> 3초뒤 알럿창</button>
     </>
   );
 }
